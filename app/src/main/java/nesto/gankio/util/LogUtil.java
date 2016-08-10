2a194268e2cacd70ba63693ad3e6a990bf42bf58
nesto <nestorm1@163.com>
2016-08-10 14:33:39 +0800

    try to fix the problem of sliding, failed..

--- app/src/main/java/nesto/gankio/util/LogUtil.java
+++ app/src/main/java/nesto/gankio/util/LogUtil.java
@@ -1,35 +1,36 @@
 package nesto.gankio.util;
 
+import android.graphics.Bitmap;
+import android.graphics.Rect;
 import android.util.Log;
 
 import nesto.gankio.BuildConfig;
 
 
-/**
- * Created by Jack_long on 2015/7/17.
- * debug模式下打印日志
- */
+@SuppressWarnings("PointlessBooleanExpression")
 public class LogUtil {
     public static final String TAG = "wtf";
 
-    public static void d(String logInfo) {
+    public static void d(Object logInfo) {
         d(TAG, logInfo);
     }
 
-    private static void d(String tag, String logInfo) {
+    public static void d(String tag, Object logInfo) {
         if (BuildConfig.DEBUG && tag != null && logInfo != null) {
-            Log.d(tag, logInfo);
+//        if (true) {
+            Log.d(tag, String.valueOf(logInfo));
         }
     }
 
-    public static void e(String logInfo) {
+    public static void e(Object logInfo) {
         e(TAG, logInfo);
     }
 
-    private static void e(String tag, String logInfo) {
+    public static void e(String tag, Object logInfo) {
         if (BuildConfig.DEBUG && tag != null && logInfo != null) {
-            Log.e(tag, getTraceInfo());
-            Log.e(tag, logInfo);
+//        if (true) {
+//            Log.e(tag, getTraceInfo());
+            Log.e(tag, String.valueOf(logInfo));
         }
     }
 
@@ -44,5 +45,28 @@ public class LogUtil {
                     .append("; number: ").append(element.getLineNumber());
         }
         return sb.toString();
+    }
+
+    public static void printLog(String log) {
+        String name = "";
+        StackTraceElement[] stacks = new Throwable().getStackTrace();
+        int stacksLen = stacks.length;
+        if (stacksLen > 2) {
+            String[] names = stacks[1].getClassName().split("\\.");
+            name = names[names.length - 1];
+        }
+        d(TAG, name + ": " + log);
+    }
+
+    public static void logWithThread(String log) {
+        d("call on " + Thread.currentThread().getName() + " " + log);
+    }
+
+    public static void logRect(Rect rect) {
+        d(rect.toString() + " height: " + rect.height() + " width: " + rect.width());
+    }
+
+    public static void logBitmap(Bitmap bitmap) {
+        d("bitmap.getWidth(): " + bitmap.getWidth() + " bitmap.getHeight(): " + bitmap.getHeight());
     }
 }
